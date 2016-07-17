ac223a312a16e994b84f40d2f89285c26c3888d2
Helen Makarchuk <helenamakarchuk@gmail.com>
2016-07-17 14:56:24 +0300

    First Commit

---
+++ visual studio 2015 project/WebApplication2/JavaScript.js
@@ -0,0 +1,72 @@
+﻿window.lastid = 0; Task(); setInterval(function () { Task(); }, 20000);
+
+$("button").click(AdditionalTask);
+
+function Task() {
+    $.ajax({
+        type: 'POST',
+        url: 'WebService1.asmx/GetNewData',
+        dataType: 'json',
+        data: JSON.stringify({ lastId: window.lastid }),
+        async: false,
+        contentType: 'application/json; charset=utf-8',
+        error: function (error) { console.log(error); },
+        success: function (data) {
+            for (i = 0; i < data.d.length; i++) {
+                var json = JSON.stringify(data.d[i]);
+                var string = JSON.parse(json);
+                var array = new Array(string.fullname, string.address);
+
+                if (string.phone != null) {
+                    var phones = (string.phone).toString();
+
+                    if (/9/i.test(phones)) {
+                        var pa = phones.split(",");
+                        var displayNumbersArray = new Array();
+
+                        for (j = 0; j < pa.length; j++) {
+                            dn = "+" + pa[j].substr(0, 1) + "(" + pa[j].substr(1, 3) + ")" + pa[j].substr(4, 3) + "-" + pa[j].substr(7, 2) + "-" + pa[j].substr(9, 2);
+                            displayNumbersArray[j] = dn;
+                        }
+                    }
+                    else { displayNumbersArray = ""; }
+                }
+                else { displayNumbersArray = ""; }
+
+                if (i != data.d.length - 1) {
+                    $('#Table tr:last').after('<tr> <td>' + array[0] + '</td> <td>' + array[1] + '</td> <td>' + displayNumbersArray + '</td> </tr>');
+                }
+                else { window.lastid = parseInt(array[0]); }
+            }
+        }
+    })
+}
+
+function AdditionalTask() {
+    buttonId = this.id;
+    $.ajax({
+        url: 'Service1.svc/F1',
+        method: 'post',
+        contentType: 'application/json; charset=utf-8',
+        data: JSON.stringify({ arg: buttonId }),
+        dataType: 'json',
+        error: function (error) { console.log(error); },
+        success: function (data) {
+            switch (buttonId) {
+                case "b1":
+                    var s = "";
+                    for (i = 0; i < data.d.length; i++) { s += data.d[i] + '\n'; }
+                    $("textarea").html(s); break;
+                case "b2": $('input').eq(0).val(data.d); break;
+                case "b3": $('input').eq(1).val(new Array(data.d)); break;
+                default: $('input').eq(2).val(data.d); 
+            }                                  
+        }
+    })
+}
+
+
+
+
+
+
---
+++ visual studio 2015 project/WebApplication2/Service1.svc.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WebApplication2
+{
+    [ServiceContract(Namespace = "")]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class Service1
+    {
+        [OperationContract]
+        public List<string> F1(string arg)
+        {
+            DataClasses1DataContext db = new DataClasses1DataContext();
+            var contacts = db.Contacts;
+            var telephones = db.Telephones;
+
+            List<string> listResult = new List<string>(); int intResult = 0; int max = -1;
+
+            if (arg == "b1") {
+                var contactsGroups = contacts.Where(b => b.BirthDate.Month > DateTime.Today.Month && b.BirthDate.Month < DateTime.Today.Month + 2).Select(p => p.FullName).ToList();
+                listResult = contactsGroups;
+            }
+            else if (arg == "b2") {
+                DateTime resultDate = new DateTime(DateTime.Today.Year - 30, DateTime.Today.Month, DateTime.Today.Day);
+
+                var contactsGroups = contacts.GroupBy(c => c.Position).Select(g => new {
+                    youngPerson = g.Where(d => d.BirthDate > resultDate).Select(d => d.Id).Count(),
+                    position = g.Key });
+
+                foreach (var group in contactsGroups) {
+                    if (group.youngPerson > max) { max = group.youngPerson; listResult.Add(group.position); }; }
+            }
+            else if (arg == "b3") {
+                var contactsGroups = contacts.GroupBy(c => c.Position).Select(g => new { b = g.Average(s => s.Salary) > contacts.Average(s => s.Salary), position = g.Key });
+                foreach (var group in contactsGroups) { if (group.b) listResult.Add(group.position); }   
+            }
+            else {
+                var contactsGroups = contacts.GroupBy(c => c.Position).Select(g => new { persons = g.Where(c => c.Salary > g.Average(s => s.Salary)).Count() });
+                foreach (var group in contactsGroups) { intResult += group.persons; }
+                listResult.Add(intResult.ToString());
+            }
+            return listResult;
+        }
+    }
+}
+
